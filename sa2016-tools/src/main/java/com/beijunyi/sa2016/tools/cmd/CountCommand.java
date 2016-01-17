5633b2fb55de5b0a5f8fdecbfb0ded16a2bb5c0c
beijunyi <beijunyi@gmail.com>
2016-01-17 19:44:01 +0000

    use set on input types

--- sa2016-tools/src/main/java/com/beijunyi/sa2016/tools/cmd/CountCommand.java
+++ sa2016-tools/src/main/java/com/beijunyi/sa2016/tools/cmd/CountCommand.java
@@ -14,7 +14,7 @@ public class CountCommand extends AbstractCommand implements Command {
   private final Set<ResourceManager> resourceManagers;
 
   @Parameter(names = "--type", variableArity = true)
-  private Set<String> types;
+  private Set<ResourceType> types;
 
   @Inject
   public CountCommand(@Nonnull EnvironmentContext context, @Nonnull Set<ResourceManager> resourceManagers) {
@@ -32,7 +32,7 @@ public class CountCommand extends AbstractCommand implements Command {
   public void call() throws IOException {
     for(ResourceManager resourceManager : resourceManagers) {
       ResourceType type = resourceManager.getResourceType();
-      if(types == null || types.contains(resourceManager.toString()))
+      if(types == null || types.contains(type))
         System.out.println("Resource " + type + ": " + resourceManager.count());
     }
   }
