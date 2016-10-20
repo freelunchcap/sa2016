1169715ea353dedc70291f17265c64595d7d66d8
beijunyi <beijunyi@gmail.com>
2016-10-21 00:02:30 +0100

    ImageRenderer

--- sa2016-tools/src/main/java/com/beijunyi/sa2016/tools/converters/ImageRenderer.java
+++ sa2016-tools/src/main/java/com/beijunyi/sa2016/tools/converters/ImageRenderer.java
@@ -1,25 +1,24 @@
 package com.beijunyi.sa2016.tools.converters;
 
 import java.awt.*;
-import java.awt.List;
+import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.*;
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
-import com.beijunyi.sa2016.tools.legacy.Palet;
-import com.beijunyi.sa2016.tools.legacy.PaletColor;
-import com.beijunyi.sa2016.tools.legacy.Real;
+import com.beijunyi.sa2016.tools.legacy.*;
 import com.beijunyi.sa2016.tools.resources.ResourceProvider;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryo.io.Input;
 import com.google.common.collect.ImmutableList;
 
 import static com.beijunyi.sa2016.tools.legacy.ClientResource.PALET;
+import static com.beijunyi.sa2016.tools.utils.BitConverter.uint8;
+import static java.lang.System.arraycopy;
 
 @Singleton
 public class ImageRenderer {
@@ -73,7 +72,116 @@ public class ImageRenderer {
 
   @Nonnull
   public RenderedImage render(Real real) {
-    throw new UnsupportedOperationException();
+    int width = real.getWidth();
+    int height = real.getHeight();
+    byte[] bitmap = new byte[width * height];
+    readBitmap(real, bitmap);
+    BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+    for(int i = 0; i < width * height; i++) {
+      int color = uint8(bitmap[i]);
+      if(color != 0) image.setRGB(i % width, i / width, colors.get(color).getRGB());
+    }
+    return image;
+  }
+
+  public static void readBitmap(Real real, byte[] bitmap) {
+    if(real.getMajor() == 1)
+      readBitmap(real.getData(), bitmap);
+    else
+      arraycopy(real.getData(), 0, bitmap, 0, bitmap.length);
+    verticalFlip(bitmap, real.getWidth(), real.getHeight());
+  }
+
+  private static void readBitmap(byte[] src, byte[] bitmap) {
+    int length = src.length;
+    int readPos = 0;
+    int writePos = 0;
+    while(readPos < length) {
+      int head = uint8(src[readPos++]);
+      byte value = 0;
+      boolean copy;
+      int x, y, z;
+      if(head >= 224) {
+        copy = false;
+        value = 0;
+        x = head - 224;
+        y = uint8(src[readPos++]);
+        z = uint8(src[readPos++]);
+      } else if(head >= 208) {
+        copy = false;
+        value = 0;
+        x = 0;
+        y = head - 208;
+        z = uint8(src[readPos++]);
+      } else if(head >= 192) {
+        copy = false;
+        value = 0;
+        x = 0;
+        y = 0;
+        z = head - 192;
+      } else if(head >= 160) {
+        copy = false;
+        value = src[readPos++];
+        x = head - 160;
+        y = uint8(src[readPos++]);
+        z = uint8(src[readPos++]);
+      } else if(head >= 144) {
+        copy = false;
+        value = src[readPos++];
+        x = 0;
+        y = head - 144;
+        z = uint8(src[readPos++]);
+      } else if(head >= 128) {
+        copy = false;
+        value = src[readPos++];
+        x = 0;
+        y = 0;
+        z = head - 128;
+      } else if(head >= 32) {
+        copy = true;
+        x = head - 32;
+        y = uint8(src[readPos++]);
+        z = uint8(src[readPos++]);
+      } else if(head >= 16) {
+        copy = true;
+        x = 0;
+        y = head - 16;
+        z = uint8(src[readPos++]);
+      } else {
+        copy = true;
+        x = 0;
+        y = 0;
+        z = head;
+      }
+      int total = x * 65536 + y * 256 + z;
+      int canWrite = bitmap.length - writePos;
+      if(total > canWrite) {
+        total = canWrite;
+      }
+      if(copy) {
+        int canRead = length - readPos;
+        if(total > canRead) {
+          total = canRead;
+        }
+        for(int i = 0; i < total; i++) {
+          value = src[readPos++];
+          bitmap[writePos++] = value;
+        }
+      } else {
+        for(int i = 0; i < total; i++) {
+          bitmap[writePos++] = value;
+        }
+      }
+    }
+  }
+
+  private static void verticalFlip(byte[] data, int width, int height) {
+    byte[] buf = new byte[width];
+    for(int i = 0; i < height / 2; i++) {
+      arraycopy(data, i * width, buf, 0, width);
+      arraycopy(data, (height - i - 1) * width, data, i * width, width);
+      arraycopy(buf, 0, data, (height - i - 1) * width, width);
+    }
   }
 
   @Nonnull
