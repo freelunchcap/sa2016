1a0645afeb157021046ed9f287efcb859884feb1
junyi.bei <junyi.bei@accenture.com>
2016-10-25 17:58:35 +0100

    dev module

--- sa2016-dev/src/main/java/com/beijunyi/sa2016/DevModule.java
+++ sa2016-dev/src/main/java/com/beijunyi/sa2016/DevModule.java
@@ -1,11 +1,12 @@
 package com.beijunyi.sa2016;
 
+import com.beijunyi.sa2016.api.ApiModule;
 import com.google.inject.AbstractModule;
 
 public class DevModule extends AbstractModule {
 
   @Override
   protected void configure() {
-
+    install(new ApiModule());
   }
 }
--- sa2016-dev/src/main/webapp/index.jsp
+++ sa2016-dev/src/main/webapp/index.jsp
@@ -1,10 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: beiju
-  Date: 2016/10/16
-  Time: 12:21
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
   <head>
